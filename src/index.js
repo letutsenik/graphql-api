42b43ac548df17abd2db72a252b451c61fb894de
Dmitri Lipski <dlipski@klika-tech.com>
2019-05-15 10:28:19 +0300

    Set up a relationship between posts and comments

--- src/index.js
+++ src/index.js
@@ -48,22 +48,26 @@ const comments = [
     {
         id: '1',
         text: 'This is awesome post. I love it',
-        author: '1'
+        author: '1',
+        post: '1'
     },
     {
         id: '2',
         text: 'Cool! I like it. Keep posting!',
-        author: '1'
+        author: '1',
+        post: '1'
     },
     {
         id: '3',
         text: 'What exactly do you mean?',
-        author: '2'
+        author: '2',
+        post: '3'
     },
     {
         id: '4',
         text: 'I like it! Thanks for your post',
-        author: '3'
+        author: '3',
+        post: '2'
     },
 ];
 
@@ -92,12 +96,14 @@ const typeDefs = `
         body: String!
         published: Boolean!
         author: User!
+        comments: [Comment!]!
     }
     
     type Comment {
         id: ID!
         text: String!
         author: User!
+        post: Post!
     }
 `;
 
@@ -149,12 +155,22 @@ const resolvers = {
             return users.find((user) => {
                 return user.id === parent.author
             })
+        },
+        comments(parent, args, ctx, info) {
+            return comments.filter((comment) => {
+                return comment.post === parent.id
+            })
         }
     },
     Comment: {
         author(parent, args, ctx, info) {
             return users.find((user) => {
                 return user.id === parent.author
+            })
+        },
+        post(parent, args, ctx, info) {
+            return posts.find((post) => {
+                return post.id === parent.post
             })
         }
     },
