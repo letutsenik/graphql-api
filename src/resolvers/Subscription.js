8947ab5759a90d5879acf166a86dcd628f87433a
dmitri_lipski <dzmitri.lipski@gmail.com>
2019-05-25 12:40:27 +0300

    Add subsciptions for edit and delete post

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -1,18 +1,4 @@
 const Subscription = {
-    count: {
-        subscribe(parent, args, { pubsub }, info) {
-            let count = 0;
-
-            setInterval(() => {
-                count++;
-                pubsub.publish('count', {
-                    count
-                })
-            }, 1000);
-
-            return pubsub.asyncIterator('count')
-        }
-    },
     post: {
         subscribe(parent, args, { pubsub }, info){
             return pubsub.asyncIterator('post')
